660bedd08bf06dd8b87ba5ca90719e7cdc3fd80d
Amanda Soto <amanda.sv01@gmail.com>
2020-10-26 20:22:11 -0500

    JavaFx controls

---
+++ Section 13/Controls/src/sample/Main.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.ResourceBundle;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //ResourceBundle resources = ResourceBundle.getBundle("Language.lang_pt");;
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
