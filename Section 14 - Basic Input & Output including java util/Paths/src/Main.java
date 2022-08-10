098462fa9599675645bce6921068c585e2a98e9b
Amanda Soto <amanda.sv01@gmail.com>
2022-08-09 23:49:34 -0500

    Filesystem paths

---
+++ Section 14 - Basic Input & Output including java util/Paths/src/Main.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) {
+        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+        printFile(path);
+        // Path filePath = FileSystems.getDefault().getPath("files","SubdirectoryFile.txt");
+        Path filePath = Paths.get(".","files","SubdirectoryFile.txt");
+        printFile(filePath);
+        filePath = Paths.get("C:\\Users\\asoto\\Documents\\JavaProgrammingMasterclass\\Section 14 - Basic Input & Output including java util\\OutThere.txt");
+        printFile(filePath);
+
+        filePath = Paths.get(".");
+        System.out.println(filePath.toAbsolutePath());
+        Path path2 = FileSystems.getDefault().getPath(".", "files", "..","files","SubdirectoryFile.txt");
+        System.out.println(path2.normalize().toAbsolutePath());
+        printFile(path2.normalize());
+    }
+
+    private static void printFile(Path path) {
+        try(BufferedReader fileReader = Files.newBufferedReader(path)) {
+            String line;
+            while((line = fileReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
