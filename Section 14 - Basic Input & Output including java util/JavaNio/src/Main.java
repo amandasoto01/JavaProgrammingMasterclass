5006fc39e012919ff44e0757f58d047ec3c0a51b
Amanda Soto <amanda.sv01@gmail.com>
2022-08-09 23:05:41 -0500

    Copy files with fileChannel and pipes

--- Section 14 - Basic Input & Output including java util/JavaNio/src/Main.java
+++ Section 14 - Basic Input & Output including java util/JavaNio/src/Main.java
@@ -1,23 +1,71 @@
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.util.Random;
+import java.nio.channels.Pipe;
 
 public class Main {
     public static void main(String[] args) {
 
-        try (FileOutputStream binFile = new FileOutputStream("data.dat");
+        try {
+            Pipe pipe = Pipe.open();
+
+            Runnable writer = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Pipe.SinkChannel sinkChannel = pipe.sink();
+                        ByteBuffer buffer = ByteBuffer.allocate(56);
+
+                        for(int i=0; i<10; i++){
+                            String currentTime = "The time is: " + System.currentTimeMillis();
+
+                            buffer.put(currentTime.getBytes());
+                            buffer.flip();
+
+                            while(buffer.hasRemaining()){
+                                sinkChannel.write(buffer);
+                            }
+                            buffer.flip();
+                            Thread.sleep(100);
+                        }
+                    } catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            Runnable reader = new Runnable() {
+                @Override
+                public void run() {
+                    try{
+                        Pipe.SourceChannel sourceChannel = pipe.source();
+                        ByteBuffer buffer = ByteBuffer.allocate(56);
+
+                        for(int i=0; i<10; i++){
+                            int bytesRead = sourceChannel.read(buffer);
+                            byte[] timeString = new byte[bytesRead];
+                            buffer.flip();
+                            buffer.get(timeString);
+                            System.out.println("Reader Thread: " + new String(timeString));
+                            buffer.flip();
+                            Thread.sleep(100);
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            new Thread(writer).start();
+            new Thread(reader).start();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+       /* try (FileOutputStream binFile = new FileOutputStream("data.dat");
              FileChannel binChannel = binFile.getChannel();
         ) {
             ByteBuffer buffer = ByteBuffer.allocate(100);
-
-            /*byte[] outputBytes = "Hello World!".getBytes();
-            byte[] outputBytes2 = "Nice to meet you".getBytes();
-            buffer.put(outputBytes).putInt(245).putInt(-98765).put(outputBytes2).putInt(1000);
-            buffer.flip();
-            */
 
             byte[] outputBytes = "Hello World!".getBytes();
             buffer.put(outputBytes);
@@ -55,39 +103,18 @@ public class Main {
 
             System.out.println("int1 = " + readBuffer.getInt());
 
-            byte[] outputString = "Hello, World!".getBytes();
-            long str1pos = 0;
-            long newInt1pos = outputString.length;
-            long newInt2pos = newInt1pos + Integer.BYTES;
-            byte[] outputString2 = "Nice to meet you".getBytes();
-            long str2pos = newInt2pos + Integer.BYTES;
-            long newInt3pos = str2pos + outputString2.length;
+            RandomAccessFile copyFile = new RandomAccessFile("datacopy.dat", "rw");
+            FileChannel copyChannel = copyFile.getChannel();
+            channel.position(0);
+            //long numTransferred = copyChannel.transferFrom(channel, 0, channel.size());
+            long numTransferred = channel.transferTo(0, channel.size(), copyChannel);
+            System.out.println("Num transferred = " + numTransferred);
 
-            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
-            intBuffer.putInt(245);
-            intBuffer.flip();
-            binChannel.position(newInt1pos);
-            binChannel.write(intBuffer);
-
-            intBuffer.flip();
-            intBuffer.putInt(-98765);
-            intBuffer.flip();
-            binChannel.position(newInt2pos);
-            binChannel.write(intBuffer);
-
-            intBuffer.flip();
-            intBuffer.putInt(1000);
-            intBuffer.flip();
-            binChannel.position(newInt3pos);
-            binChannel.write(intBuffer);
-
-            binChannel.position(str1pos);
-            binChannel.write(ByteBuffer.wrap(outputString));
-            binChannel.position(str2pos);
-            binChannel.write(ByteBuffer.wrap(outputString2));
-
+            channel.close();
+            ra.close();
+            copyChannel.close();
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 }
