45052e42338c1faa04d0567d3386275a681a4043
Amanda Soto <amanda.sv01@gmail.com>
2022-08-13 17:23:40 -0500

    Added copy file

--- Section 14 - Basic Input & Output including java util/Paths/src/Main.java
+++ Section 14 - Basic Input & Output including java util/Paths/src/Main.java
@@ -8,7 +8,17 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+
+        try {
+            Path sourceFile = FileSystems.getDefault().getPath("Examples","file1.txt");
+            Path copyFile = FileSystems.getDefault().getPath("Examples","file1copy.txt");
+            Files.copy(sourceFile,copyFile);
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+        /*Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
         printFile(path);
         // Path filePath = FileSystems.getDefault().getPath("files","SubdirectoryFile.txt");
         Path filePath = Paths.get(".","files","SubdirectoryFile.txt");
@@ -21,7 +31,18 @@ public class Main {
         Path path2 = FileSystems.getDefault().getPath(".", "files", "..","files","SubdirectoryFile.txt");
         System.out.println(path2.normalize().toAbsolutePath());
         printFile(path2.normalize());
-    }
+
+        Path path3 = FileSystems.getDefault().getPath("thisfiledoesnotexist.txt");
+        System.out.println(path3.toAbsolutePath());
+
+        Path path4 = Paths.get("z:\\tests\\ING","abcdef", "whatever.txt");
+        System.out.println(path4.toAbsolutePath());
+
+        filePath = FileSystems.getDefault().getPath("files");
+        System.out.println("Exists = " + Files.exists(filePath));
+
+        System.out.println("Exists = " + Files.exists(path4));
+    }*/
 
     private static void printFile(Path path) {
         try(BufferedReader fileReader = Files.newBufferedReader(path)) {
