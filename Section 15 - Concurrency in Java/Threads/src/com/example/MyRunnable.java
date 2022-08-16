c586fea02721ef9c77512933c86e97e56894af33
Amanda Soto <amanda.sv01@gmail.com>
2022-08-15 21:24:07 -0500

    Runnable and thread

---
+++ Section 15 - Concurrency in Java/Threads/src/com/example/MyRunnable.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import static com.example.ThreadColor.ANSI_RED;
+
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println(ANSI_RED + "Hello from MyRunnable's implementation of run()");
+    }
+}
